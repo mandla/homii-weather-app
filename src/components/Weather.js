2b856abf8f117407f03aa5efdb8447b993e444bc
mandla <aldnammandla@live.com>
2023-01-22 11:03:05 +0200

    feat(weather): finishing up

---
+++ src/components/Weather.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image} from 'react-native'
+import { colors } from '../utils/utility'
+
+const { PRIMARY_COLOR } = colors
+
+export default function Weather({ currentWeatherDetails }) {
+    const {
+        main: { temp },
+        name } = currentWeatherDetails;
+
+    return (
+        <View style={styles.textContainer}>
+            <View style={styles.topText}> 
+                <Text style={styles.textColor}>{name}</Text>
+            </View>
+            <View style={styles.bottomText}> 
+                <Text style={styles.textColor} >{temp}°</Text>
+            </View>
+            <Image style={styles.bottomImage} source={require('../../assets/sun-image.png')} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    weatherInfo: {
+        alignItems: 'center'
+    },
+    textColor: {
+        fontSize: 40,
+        color: PRIMARY_COLOR
+    },
+    textContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+    },
+    topText: {
+        position: 'absolute',
+        top: '33%'
+    },
+    bottomText: {
+        position: 'absolute',
+        bottom: '33%'
+    },
+    bottomImage: {
+        position: 'absolute',
+        bottom: '13%',
+        resizeMode: 'contain',
+        height: 100,
+        width: 100
+    }
+})
---
+++ src/utils/utility.js
@@ -0,0 +1,5 @@
+export const colors = {
+    PRIMARY_COLOR: '#807e7e',
+    SECONDARY_COLOR: '#002244',
+    BORDER_COLOR: '#424242',
+}
